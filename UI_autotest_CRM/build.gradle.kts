ff8a8bc708d52bf39a5b7b3d1e7a57601c941547
burrbraun <burrbraun@gmail.com>
2023-07-03 20:41:29 +0300

    moved suite to level up to Kotlin folder for test

--- UI_autotest_CRM/build.gradle.kts
+++ UI_autotest_CRM/build.gradle.kts
@@ -12,18 +12,14 @@ repositories {
 
 dependencies {
     implementation ("com.codeborne:selenide:6.13.1")
-    implementation ("org.junit.jupiter:junit-jupiter:5.9.0")
-    implementation ("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    implementation ("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("junit:junit:4.11")
     testImplementation(kotlin("test"))
     implementation ("commons-io:commons-io:2.11.0")
     implementation ("mysql:mysql-connector-java:8.0.19")
     implementation ("com.opencsv:opencsv:5.3")
     testImplementation("org.testng:testng:7.1.0")
     testImplementation("org.testng:testng:7.1.0")
-    testImplementation ("org.junit.platform:junit-platform-suite-api:1.7.0")
-    testImplementation ("org.junit.platform:junit-platform-runner:1.2.0")
-    implementation ("org.junit.platform:junit-platform-suite-engine:1.10.0-M1")
+
 
 }
 
--- UI_autotest_CRM/src/test/kotlin/CrmTestsSuite.kt
+++ UI_autotest_CRM/src/test/kotlin/CrmTestsSuite.kt
@@ -1,15 +1,14 @@
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
-import org.junit.platform.suite.api.SelectClasses
-import org.junit.platform.suite.api.Suite
 import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 
-
-@Suite
-@SelectClasses(BaseTest::class, CRMCallsTest::class, CRMSmokeTest::class, CRMWifiAccessTest::class)
+@RunWith(Suite::class)
+@Suite.SuiteClasses(CRMCallsTest::class, CRMSmokeTest::class, CRMWifiAccessTest::class)
 class CrmTestsSuite {
 }
