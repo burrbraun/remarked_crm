62c918f80ad5112fdeb54ec75dca5191e0a7bf4b
burrbraun <burrbraun@gmail.com>
2023-07-17 20:26:54 +0300

    T changed to t in results folder in base test

--- UI_autotest_CRM/src/test/kotlin/BaseTest.kt
+++ UI_autotest_CRM/src/test/kotlin/BaseTest.kt
@@ -73,8 +73,8 @@ open class BaseTest {
         //driver[baseUrl]
 
         fileDownload = FOLDER
-        downloadsFolder = "src/test/resources/TestResults"
-        System.setProperty("selenide.reportsFolder", "src/test/resources/TestResults")
+        downloadsFolder = "src/test/resources/testResults"
+        System.setProperty("selenide.reportsFolder", "src/test/resources/testResults")
        // open(baseUrl)
         timeout = 300000
         open(baseUrl)
